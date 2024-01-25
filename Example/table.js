aed74ab14326df4ae54854bd7d8f9b72e47639dc
Nachos-Ph <nicosbaltazar@gmail.com>
2024-01-25 15:02:57 +0800

    Jan 25 24 - Hotdog

--- Example/table.js
+++ Example/table.js
@@ -5,4 +5,19 @@ function insertrow() {
     let cell2 = row.insertCell(1);
     let cell3 = row.insertCell(2);
     let cell4 = row.insertCell(3);
+
+    let lastname = document.getElementById("lastname").value;
+    let firstname = document.getElementById("firstname").value;
+    let age = document.getElementById("age").value;
+    let action = document.getElementById("action").value;
+
+    cell1.innerHTML = lastname;
+    cell2.innerHTML = firstname;
+    cell3.innerHTML = age;
+    cell4.innerHTML = "<button onclick='deleterow()'>Delete Row</button>";
+}
+
+function deleterow(r){
+    let i = r.parentNode.parentNode.rowIndex;
+    document.getElementById("mytable").deleteRow(i);
 }
