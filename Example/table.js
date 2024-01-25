3973c09c9a3333e4f11938753833235633dfe570
Nachos-Ph <nicosbaltazar@gmail.com>
2024-01-25 14:39:31 +0800

    Jan 25, 2024 - Added Example Tables

---
+++ Example/table.js
@@ -0,0 +1,8 @@
+function insertrow() {
+    let table = document.getElementById("mytable");
+    let row = table.insertRow(1);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
+}
